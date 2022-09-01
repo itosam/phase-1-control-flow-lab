8d7c06ac2d347c13690e2f89eb887ec392318a81
Osamu Ito <mr.samito@gmail.com>
2022-08-31 21:59:33 -0400

    commit tree 49db2190dd0d9fb91a2ca68320a238182d7e3276

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+
+  if (someNumber <= 400) {
+    return 'This one is on me!';
+  } else if (someNumber <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if(someNumber > 2000 && someNumber <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {someNumber > 2500} {
+    return 'No can do.'
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  return city === 'NYC'?'Ok, sounds good.' : 'No go.'
+  }
   // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous' :
+    return 'Thank you.'
+   default:
+    return 'Bye.'
+  }
 }
